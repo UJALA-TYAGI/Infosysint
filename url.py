7d7c70ba9a6a6306ce640f46abde676345f3a18d
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-05-17 11:41:26 +0530

    Update url.py

--- url.py
+++ url.py
@@ -9,28 +9,13 @@ def urlinfo():
     inurl=input("URL >> ")
     print('')
     # print('url >> '+inurl)
-    api_key = 'ff86a9bedea4147d45fb4e5e9bf6756a861bc1ec'
-    # url = 'https://www.virustotal.com/vtapi/v2/url/scan'
-    # params = {'apikey': 'b5729b641e9d33f2bdf667c2eafb57e493250e0b3a3d602c1337be3eeecd7f74', 'url':inurl}
+    api_key = 'your api key'
 
-    # response = requests.post(url, data=params)
-    # dictres=response.json()
-    # print('')
-    # scanid=dictres['scan_id']
     print("[+] Checking URl......")
-    # scanurl = 'https://www.virustotal.com/vtapi/v2/url/report'
-    # params = {'apikey': 'b5729b641e9d33f2bdf667c2eafb57e493250e0b3a3d602c1337be3eeecd7f74', 'resource': scanid }
+
     scanurl = 'https://endpoint.apivoid.com/urlrep/v1/pay-as-you-go/?key='+api_key+'&url=https%3A%2F%2Fwww.google.com%2F'
     response = requests.get(scanurl)
     dict=response.json()
-    #dict = json.loads(dictreport)
-    # print('Total Scanning >> ',dictreport['total'])
-    # sites=dictreport['scans']
-    # for x in sites:
-    #     print(BOLD+x)
-    #     #print(sites[x])
-    #     for y in sites[x]:
-    #         print("       ",y," : ",sites[x][y])
 
     mydict1 = []
 
